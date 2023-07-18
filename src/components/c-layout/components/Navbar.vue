af3f26bc0c61ed7575d856fefe83e175142101a1
chenjie <785801610@qq.com>
2023-07-18 23:27:02 +0800

    feat: 新增 navbar 子组件展示

--- src/components/c-layout/components/Navbar.vue
+++ src/components/c-layout/components/Navbar.vue
@@ -1,7 +1,62 @@
 <template>
-  <div class="navbar"> Navbar </div>
+  <div class="navbar">
+    <div class="right">
+      <el-dropdown trigger="click" class="dropdown">
+        <div>
+          <el-avatar
+            shape="square"
+            class="avatar"
+            :src="userStore.current?.avatar"
+          ></el-avatar>
+          <el-icon>
+            <Setting />
+          </el-icon>
+        </div>
+        <template #dropdown>
+          <el-dropdown-menu>
+            <RouterLink to="/">
+              <el-dropdown-item>主页</el-dropdown-item>
+            </RouterLink>
+            <el-dropdown-item :divided="true">退出登录</el-dropdown-item>
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
+    </div>
+  </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+  import { useUserStore } from '@/store'
+  import { RouterLink } from 'vue-router'
+  import { Setting } from '@element-plus/icons-vue'
 
-<style lang="scss" scoped></style>
+  const userStore = useUserStore()
+</script>
+
+<style lang="scss" scoped>
+  .navbar {
+    height: 50px;
+    background: #fff;
+    box-shadow: 0 1px 4px rgb(0 21 41 / 8%);
+
+    .right {
+      height: 100%;
+      display: flex;
+      align-items: center;
+      float: right;
+      padding-right: 16px;
+
+      .dropdown {
+        cursor: pointer;
+
+        .avatar {
+          margin-right: 5px;
+        }
+      }
+    }
+  }
+
+  :deep(.router-link-active) {
+    text-decoration: none;
+  }
+</style>
