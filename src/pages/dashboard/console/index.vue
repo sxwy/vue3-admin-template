f9f77c5e3fb2773e984daaa449123bd2dffa7698
chenjie <785801610@qq.com>
2023-08-02 00:02:41 +0800

    chore: 新增调试代码

--- src/pages/dashboard/console/index.vue
+++ src/pages/dashboard/console/index.vue
@@ -2,6 +2,12 @@
   <div>
     console/index
 
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
     <el-pagination :total="100" />
   </div>
 </template>
