d4c27cac7ae06026832b31c761f2c2eae4b583c8
chenjie <785801610@qq.com>
2023-08-12 23:27:44 +0800

    feat: 新增列表页面样式

--- src/pages/system/role/index.vue
+++ src/pages/system/role/index.vue
@@ -1,7 +1,23 @@
 <template>
-  <div> role/index </div>
+  <div>
+    <div class="form"> 搜索 </div>
+    <div class="table"> 表格 </div>
+  </div>
 </template>
 
 <script lang="ts" setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  .form {
+    padding: 15px;
+    background-color: #fff;
+    border-radius: 6px;
+  }
+
+  .table {
+    padding: 15px;
+    background-color: #fff;
+    border-radius: 6px;
+    margin-top: 20px;
+  }
+</style>
