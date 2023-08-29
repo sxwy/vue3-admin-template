1c2ee50ebd90378bc52c51b5276ed26aa3577064
chenjie <785801610@qq.com>
2023-08-29 23:51:18 +0800

    feat: 调整路由配置

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { BASE_SITE } from '@/constants'
 import {
   basicsRoutes,
@@ -12,7 +12,7 @@ export * from './modules'
 const routes = [...basicsRoutes, exceptionRoutes, loginRoutes, dashboardRoutes]
 
 const router = createRouter({
-  history: createWebHistory(BASE_SITE),
+  history: createWebHashHistory(BASE_SITE),
   routes
 })
 
