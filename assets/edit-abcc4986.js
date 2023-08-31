f2ad4130082b58b2e7c99e07ee33faaa2c38024e
webchenjie <53405269+webchenjie@users.noreply.github.com>
2023-08-31 08:51:47 +0000

    Deploying to gh-pages from @ sxwy/vue3-admin-template@1375df5dc628e9d5cd8f1ee3e9d5788912664195 🚀

---
+++ assets/edit-abcc4986.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as t}from"./index-4599bba3.js";const o={};function n(r,s){return c(),t("div",null," video/edit ")}const a=e(o,[["render",n]]);export{a as default};
--- assets/index-legacy-a619c108.js
+++
@@ -1,1 +0,0 @@
-System.register(["./index-legacy-68ba30c9.js"],(function(e,t){"use strict";var n,r,u;return{setters:[e=>{n=e._,r=e.o,u=e.c}],execute:function(){e("default",n({},[["render",function(e,t){return r(),u("div",null," user/index ")}]]))}}}));
